b6ed36c2cc87ad6d32b362209186804cf3bed521
Arian Hadi <arianhadi2003@gmail.com>
2025-05-31 13:24:49 +0300

    update include smtp email

--- backend/meta_core/settings.py
+++ backend/meta_core/settings.py
@@ -107,3 +107,20 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
+
+
+AUTHENTICATION_BACKENDS = [
+    'accounts.authentication.EmailBackend',
+    'django.contrib.auth.backends.ModelBackend',
+]
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = 'optimusprime2082@gmail.com'
+EMAIL_HOST_PASSWORD = 'yyey tijk uycr csev'
+
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS') 
