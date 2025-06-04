8b041a9b27f93d39b97110bb57f9484f6a66787f
Arian Hadi <arianhadi2003@gmail.com>
2025-06-04 13:12:18 +0300

    update product detail page initial phase

--- backend/products/views.py
+++ backend/products/views.py
@@ -3,12 +3,12 @@ from .models import Product
 
 class ProductListView(ListView):
     model = Product
-    template_name = 'product_list.html'  # you can also name it 'products/product_list.html'
+    template_name = 'products/product_list.html'  # you can also name it 'products/product_list.html'
     context_object_name = 'products'
 
 class ProductDetailView(DetailView):
     model = Product
-    template_name = 'product_detail.html'
+    template_name = 'products/product_detail.html'
     context_object_name = 'product'
     slug_field = 'slug'
     slug_url_kwarg = 'slug'
