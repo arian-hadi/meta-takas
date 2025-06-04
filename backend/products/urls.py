5608a618c96e2b14e05f21bf9bcde42070f4434e
Arian Hadi <arianhadi2003@gmail.com>
2025-06-04 16:56:23 +0300

    update working filter

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -5,4 +5,5 @@ from .views import *
 urlpatterns = [
     path('', ProductListView.as_view(), name='product_list'),
     path('<slug:slug>/', ProductDetailView.as_view(), name='product_detail'),
+    path('category/<slug:category_slug>/', ProductListView.as_view(), name='product_list_by_category'),
 ]
--- backend/products/views.py
+++ backend/products/views.py
@@ -1,10 +1,22 @@
 from django.views.generic import ListView, DetailView
-from .models import Product
+from .models import Product, Category
 
 class ProductListView(ListView):
     model = Product
     template_name = 'products/product_list.html'  # you can also name it 'products/product_list.html'
     context_object_name = 'products'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        category_slug = self.kwargs.get('category_slug')
+        if category_slug:
+            queryset = queryset.filter(category__slug=category_slug)
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        return context
 
 class ProductDetailView(DetailView):
     model = Product
