77d65aed6380f9de1922f2d95e01f9029502469d
Arian Hadi <arianhadi2003@gmail.com>
2025-06-13 14:53:54 +0300

    update filters are updated

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,16 +1,19 @@
 from django.views.generic import ListView, DetailView
 from .models import Product, Category
-from django.db.models import Q
+from django.db.models import Q, Count
+
 
 class ProductListView(ListView):
     model = Product
-    template_name = 'products/product_list.html'  # you can also name it 'products/product_list.html'
+    template_name = 'products/product_list.html'
     context_object_name = 'products'
 
     types = [
-    {'slug': 'sale', 'label': 'Satılık'},
-    {'slug': 'exchange', 'label': 'Takas'},
+        {'slug': 'sale', 'label': 'Satılık'},
+        {'slug': 'exchange', 'label': 'Takas'},
     ]
+
+
 
     def get_queryset(self):
         queryset = super().get_queryset()
@@ -21,20 +24,24 @@ class ProductListView(ListView):
                 Q(name__icontains=search_query) |
                 Q(description__icontains=search_query) |
                 Q(exchange_for__icontains=search_query)
-    )
-        
-        self.category_slug = self.kwargs.get('category_slug')  # ← this line is added
-        listing_type = self.request.GET.get('type')  # 'sale' or 'exchange'
-        self.sort_by = self.request.GET.get('sort')
-        price_min = self.request.GET.get('price_min')
-        price_max = self.request.GET.get('price_max')
+            )
 
+        self.category_slug = self.kwargs.get('category_slug')
+
+        selected_categories = self.request.GET.getlist('category')
+        selected_types = self.request.GET.getlist('type')
+        price_min = self.request.GET.get('min_price')
+        price_max = self.request.GET.get('max_price')
+        self.sort_by = self.request.GET.get('sort')
 
         if self.category_slug:
             queryset = queryset.filter(category__slug=self.category_slug)
-        
-        if listing_type in ['sale', 'exchange']:
-            queryset = queryset.filter(listing_type=listing_type)
+
+        if selected_categories:
+            queryset = queryset.filter(category__slug__in=selected_categories)
+
+        if selected_types:
+            queryset = queryset.filter(listing_type__in=selected_types)
 
         if price_min:
             queryset = queryset.filter(price__gte=price_min)
@@ -42,34 +49,48 @@ class ProductListView(ListView):
         if price_max:
             queryset = queryset.filter(price__lte=price_max)
 
-
+        # Apply sorting
         if self.sort_by == 'price_asc':
-            # Only apply price sorting to 'sale' products
-            if self.listing_type == 'sale':
-                queryset = queryset.order_by('price')
+            queryset = queryset.order_by('price')
         elif self.sort_by == 'price_desc':
-            if self.listing_type == 'sale':
-                queryset = queryset.order_by('-price')
+            queryset = queryset.order_by('-price')
         elif self.sort_by == 'name_asc':
             queryset = queryset.order_by('name')
         else:
-            queryset = queryset.order_by('-created_at')  # Default: newest
-            
-        
+            queryset = queryset.order_by('-created_at')  # Default
+
         return queryset
-    
-    def get_context_data(self, **kwargs):
+
+    def get_context_data(self, **kwargs):   
         context = super().get_context_data(**kwargs)
+        
         context['categories'] = Category.objects.all()
+        
+        listing_type_counts = Product.objects.values('listing_type').annotate(count=Count('id'))
+        count_map = {item['listing_type']: item['count'] for item in listing_type_counts}
+
+        types_with_counts = []
+        for t in self.types:
+            types_with_counts.append({
+                'slug': t['slug'],
+                'label': t['label'],
+                'count': count_map.get(t['slug'], 0)
+            })
+
+        
+        context['types'] = types_with_counts
         context['current_category_slug'] = self.category_slug
-        context['current_listing_type'] = self.request.GET.get('type')
+
+        # Send selected filters back to template
+        context['selected_categories'] = self.request.GET.getlist('category')
+        context['selected_types'] = self.request.GET.getlist('type')
+        context['min_price'] = self.request.GET.get('min_price', '')
+        context['max_price'] = self.request.GET.get('max_price', '')
         context['current_sort'] = self.request.GET.get('sort')
         context['search_query'] = self.request.GET.get('q', '')
+        context['listing_types'] = self.types
+        context['listing_type_counts'] = count_map
 
-        context['types'] = [
-        {'slug': 'sale', 'label': 'Satılık'},
-        {'slug': 'exchange', 'label': 'Takas'},
-        ]
 
         return context
 
