6f37357fdb46f52ff758d47454fbbcb8196898b6
Arian Hadi <arianhadi2003@gmail.com>
2025-06-13 18:24:02 +0300

    update the main page

--- backend/homepage/views.py
+++ backend/homepage/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from products.models import Product
 
 
 class HomeView(TemplateView):
     template_name = "homepage/homepage.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['latest_products'] = Product.objects.order_by('-created_at')[:4]
+        return context
 
 class AboutUsView(TemplateView):
     template_name = "homepage/aboutus.html"
