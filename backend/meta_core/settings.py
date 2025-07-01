eb220f5257f0ca817bf603b7b987982f224d213e
Arian Hadi <arianhadi2003@gmail.com>
2025-07-01 10:21:06 +0300

    fix the navbar pages are accessible

--- backend/meta_core/settings.py
+++ backend/meta_core/settings.py
@@ -50,7 +50,7 @@ ROOT_URLCONF = 'meta_core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'theme' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
