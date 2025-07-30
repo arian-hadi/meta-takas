a1f75cca877932201637027d34312b1b9ee181b3
Arian Hadi <arianhadi2003@gmail.com>
2025-07-30 17:51:29 +0300

    update the redis setup

--- backend/meta_core/settings.py
+++ backend/meta_core/settings.py
@@ -145,3 +145,14 @@ EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS') 
 DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_USER')
 
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://redis:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
