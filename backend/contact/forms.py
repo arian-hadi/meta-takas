08870801b006afd1d2f4b87b38ed4577546fb3e1
Arian Hadi <arianhadi2003@gmail.com>
2025-08-05 18:13:42 +0300

    fix footer page height from the main content

--- backend/contact/forms.py
+++ backend/contact/forms.py
@@ -1,14 +1,15 @@
 from django import forms
+from django.utils.translation import gettext_lazy as _
 
 class ContactForm(forms.Form):
     name = forms.CharField(max_length=100)
     email = forms.EmailField()
     subject = forms.ChoiceField(choices=[
-        ('', 'Lütfen bir kategori seçin'),
-        ('delivery', 'Teslimat'),
-        ('support', ' Destek'),
-        ('profile', 'Profil'),
-        ('careers', ' Kariyer'),
-        ('another', 'Diğer kategori'),
+        ('', _("Please select a category")),
+        ('delivery', _("Delivery")),
+        ('support', _("Support")),
+        ('profile', _("Profile")),
+        ('careers', _("Careers")),
+        ('another', _("Other category")),
     ])
     message = forms.CharField(widget=forms.Textarea)
